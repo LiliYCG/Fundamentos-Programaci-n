c91789ae546310a8d581310ec2d3d2fd1a818b9e
LiliYCG <143724416+LiliYCG@users.noreply.github.com>
2023-09-14 09:16:09 -0500

    Add files via upload

---
+++ Practica4.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica4;
+import java.util.Scanner;
+/**
+ *
+ * @author labcca
+ */
+public class Practica4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        double Dolar = 17.16;
+        double Euro = 18.30;
+       
+        Scanner practica = new Scanner (System.in);
+        System.out.println("Ingresa cantidad ");
+        int pesos = practica.nextInt();
+        
+        double dolares = ( pesos / 17);
+       
+        
+        double euros = (pesos / 18);
+       
+       practica.close();
+       
+        System.out.println("pesos a dolares: " + dolares);
+        System.out.println("pesos a euros: " + euros);
+       
+        
+        
+        
+        
+    }
+    
+}
